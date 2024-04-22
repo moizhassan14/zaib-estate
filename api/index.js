ff0f72099e074d29f849b9019f18ecc35a117a68
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-04-22 12:55:51 +0500

    create Sign up api route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import 'dotenv/config';
 import chalk from 'chalk';
-import userRoutes from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 mongoose.connect(process.env.MONGODB).then(() => {
   console.log(chalk.blue.bgGreen.bold('Connected to MongoDB sucessfully !!!'));
@@ -12,8 +13,10 @@ mongoose.connect(process.env.MONGODB).then(() => {
 
 const app = express();
 
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server running on port 3000!!!");
 });
 
-app.use('/api/user',userRoutes);
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
