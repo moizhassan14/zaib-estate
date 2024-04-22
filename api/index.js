0b47a5d3a08d5211a1873f78229629c5f69539d7
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-04-22 11:12:27 +0500

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config';
+import chalk from 'chalk';
+
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log(chalk.blue.bgGreen.bold('Connected to MongoDB sucessfully !!!'));
+}).catch((err) => {
+  console.log(err);
+})
+
 const app = express();
 
 app.listen(3000, () => {
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
             </li>
           </Link>
           <Link to="/sign-in">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className=" sm:inline text-slate-700 hover:underline">
               Sign in
             </li>
           </Link>
