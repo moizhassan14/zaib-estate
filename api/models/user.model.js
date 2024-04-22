66d16d544b77238a2bd0f7bf63054e9b873e6e89
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-04-22 11:34:15 +0500

    create the User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},{timestamps:true});
+
+const User = mongoose.model("User",userSchema,'zaib-estate-USERS');//collection name zaib-estate-USERS
+export default User
