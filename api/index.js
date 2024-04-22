99149c1320d5a1806ffde1f4371bf6f9219add67
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-04-22 11:53:08 +0500

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import 'dotenv/config';
 import chalk from 'chalk';
+import userRoutes from './routes/user.route.js'
 
 mongoose.connect(process.env.MONGODB).then(() => {
   console.log(chalk.blue.bgGreen.bold('Connected to MongoDB sucessfully !!!'));
@@ -14,3 +15,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server running on port 3000!!!");
 });
+
+app.use('/api/user',userRoutes);
