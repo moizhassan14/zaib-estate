0c302ef3fe5be244c24b9b48e296bee14c041232
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-04-24 14:42:44 +0500

    added cors to the backend and complete the signup functionality

--- api/index.js
+++ api/index.js
@@ -4,30 +4,33 @@ import 'dotenv/config';
 import chalk from 'chalk';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from "cors";
 
 mongoose.connect(process.env.MONGODB).then(() => {
-  console.log(chalk.blue.bgGreen.bold('Connected to MongoDB sucessfully !!!'));
+  console.log(chalk.blue.bgGreen.bold('Connected to MongoDB successfully !!!'));
 }).catch((err) => {
   console.log(err);
 })
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.listen(3000, () => {
-  console.log("Server running on port 3000!!!");
-});
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
-//middleware
-app.use((err,req,res,next,)=>{
-  const statusCode=err.statusCode || 500;
-  const message=err.message||'Internal Server Error';
+// Middleware for handling errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message
   });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000!!!");
 });
