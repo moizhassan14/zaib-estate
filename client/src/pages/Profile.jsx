26bca7523636ca5196c957c265d68df7e5ca897c
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-04-28 15:48:07 +0500

    complete image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,26 +1,126 @@
-import {useSelector} from 'react-redux';
+import { useSelector } from "react-redux";
+import { useRef, useState, useEffect } from "react";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "../firebase";
+
 export default function Profile() {
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUplaodError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+  // console.log(formData);
+  // console.log(filePerc);
+  // console.log(fileUplaodError);
+
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = async (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        // console.log("Upload is " + progress + "% done");
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+  };
   return (
-    <div className='p-3 max-w-lg mx-auto'>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
-      <form className='flex flex-col gap-4' >
-        <img className='rounded-full object-cover w-24 h-23 self-center mt-2' src={currentUser.avatar} alt="profile" />
-        <input type="text" placeholder="username"
-        className='border rounded-lg p-3'
-        id='username' />
-        <input type="email" placeholder="email"
-        className='border rounded-lg p-3'
-        id='email' />
-        <input type="password" placeholder="password"
-        className='border rounded-lg p-3'
-        id='password' />
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+      <form className="flex flex-col gap-4">
+        <input
+          onChange={(e) => setFile(e.target.files[0])}
+          hidden
+          type="file"
+          ref={fileRef}
+          accept="image/*"
+        />
+        <img
+          onClick={() => fileRef.current.click()}
+          className="rounded-full object-cover w-24 h-23 self-center mt-2"
+          src={formData.avatar ||currentUser.avatar}
+          alt="profile"
+        />
+        <p className="self-center text-sm">
+          {fileUplaodError ? (
+            <span className="text-red-700">Error Image Uplaod</span>
+          ) : filePerc > 0 && filePerc < 100 ? (
+            <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+          ) : filePerc === 100 ? (
+            <span className="text-green-700">Image uploaded succesfully</span>
+          ) : (
+            ""
+          )}
+        </p>
+        <input
+          type="text"
+          placeholder="username"
+          className="border rounded-lg p-3"
+          id="username"
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="border rounded-lg p-3"
+          id="email"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="border rounded-lg p-3"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
       </form>
-      <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span className='text-red-700 cursor-pointer'>Sign out</span>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 cursor-pointer">Delete Account</span>
+        <span className="text-red-700 cursor-pointer">Sign out</span>
       </div>
     </div>
-  )
+  );
 }
+
+//Firebase rule
+
+// rules_version = '2';
+
+// // Craft rules based on data in your Firestore database
+// // allow write: if firestore.get(
+// //    /databases/(default)/documents/users/$(request.auth.uid)).data.isAdmin;
+// service firebase.storage {
+//   match /b/{bucket}/o {
+//     match /{allPaths=**} {
+//       allow read;
+//       allow write : if
+//       request.resource.size< 2 *1024*1024 &&
+//       request.resource.contentType.matches('image/.*')
+//     }
+//   }
+//
