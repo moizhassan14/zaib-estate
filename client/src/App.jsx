663a3010f8a43501d296cb5202da703e72bbd28b
MoizHassan <118419901+moizhassan14@users.noreply.github.com>
2024-05-11 01:58:28 +0500

    completed the rest of the listing functionality page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/createListing";
 import UpdateListing from "./pages/updateListing";
-import Listings from "./pages/Listings";
+import Listing from "./pages/Listings";
 
 export default function App() {
   return (
@@ -19,7 +19,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/listing/:listingId" element={<Listings />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
